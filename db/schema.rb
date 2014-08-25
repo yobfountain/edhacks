d6f35833ed5e7e5cb59bf49b72b0d754e2b9b8ae
Aidan Feldman <aidan.feldman@gmail.com>
2014-08-25 00:56:39 -0400

    update schema file

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@
 ActiveRecord::Schema.define(version: 20140822020825) do
 
   create_table "admins", force: true do |t|
+    t.string   "name"
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
